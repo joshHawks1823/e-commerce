1f83065af1a91f88326349ad4139698896983484
joshhawks1823 <jheat23@gmail.com>
2021-02-03 12:33:57 -0500

    New Commit

---
+++ frontend/src/products.js
@@ -0,0 +1,74 @@
+
+const products = [
+
+
+  {
+    _id: '1',
+    name: 'The Classic Toupee',
+    image: '/images/toupee1.jpg',
+    description:
+      'Thinning a little? This toupee will give you that youthful vigor that you have been missing.',
+    price: 39.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'The Damsel in Distress',
+    image: '/images/damsel.jpg',
+    description:
+      'She will be irresistible enough to storm any bastille to rescue her, this wig is like something out of a fairy tale.',
+    price: 49.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Glamour Girl',
+    image: '/images/elegance1.jpg',
+    description:
+      'Stunning, elegant, and glamorous, ask about the pearls.',
+    price: 59.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'The Mullet',
+    image: '/images/mullet1.jpg',
+    description:
+      'Rock out with your Tennessee top hat, business in the front... party in the back.',
+    price: 39.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'The Bob Marley',
+    image: '/images/rastafari1.jpg',
+    description:
+      'Feel the vibration, and the wind flowing through your locks, one love.',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'The Afro',
+    image: '/images/afro1.jpg',
+    description:
+      'From disco to soul, go natural or cultivate your fro.',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+
+export default products
